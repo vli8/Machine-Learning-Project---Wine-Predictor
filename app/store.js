f5f165a5b21a70713d67787de43d2742a3b458a6
vli8 <vli8@fordham.edu>
2019-04-01 15:09:02 -0400

    setting up database

--- app/store.js
+++ app/store.js
@@ -15,3 +15,15 @@ const middleware = composeWithDevTools(
 );
 const store = createStore(reducer, middleware);
 export default store;
+// {"points": "87",
+//   "title": "Nicosia 2013 Vulk\u00e0 Bianco  (Etna)",
+//   "description": "Aromas include tropical fruit, broom, brimstone and dried herb. The palate isn't overly expressive, offering unripened apple, citrus and dried sage alongside brisk acidity.",
+//   "taster_name": "Kerin O\u2019Keefe",
+//   "taster_twitter_handle": "@kerinokeefe",
+//   "price": 15,
+//   "designation": "Vulk\u00e0 Bianco",
+//   "variety": "White Blend",
+//   "region_1": "Etna", "region_2": null,
+//   "province": "Sicily & Sardinia",
+//   "country": "Italy",
+//   "winery": "Nicosia"}
