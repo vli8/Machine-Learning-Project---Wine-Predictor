fe52027a5c31f8ec1bc3d16051d4cc69f9952cb2
vli8 <vli8@fordham.edu>
2019-04-08 15:47:05 -0400

    routes server added

--- server/index.js
+++ server/index.js
@@ -12,6 +12,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "..", "public")));
 
+app.use("/api", require("./api"));
+
 let counter = 0;
 const startServerConnectDB = async () => {
   await db.sync({ force: true });
