263eb1b7142f0cb0e2241170eee04918c18cdc7f
vli8 <vli8@fordham.edu>
2019-04-12 14:30:14 -0400

    machine learning brain js added

--- app/components/PredictWine.js
+++ app/components/PredictWine.js
@@ -4,6 +4,7 @@ import Jumbotron from "react-bootstrap/Jumbotron";
 import Button from "react-bootstrap/Button";
 import { Link } from "react-router-dom";
 import { addWine } from "../store/wine";
+import brain from "brain.js";
 
 class PredictWine extends React.Component {
   constructor() {
@@ -15,7 +16,9 @@ class PredictWine extends React.Component {
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
+
   componentDidMount() {}
+
   async handleChange(event) {
     if (event.target.name === "description") {
       await this.setState({
@@ -28,12 +31,27 @@ class PredictWine extends React.Component {
     }
     console.log("state: ", this.state);
   }
+
+  trainData() {
+    const network = new brain.NeuralNetwork();
+    network.train([
+      { input: [0, 0, 0], output: [0] },
+      { input: [0, 0, 1], output: [0] },
+      { input: [1, 0, 0], output: [1] },
+      { input: [1, 1, 0], output: [1] }
+    ]);
+    const output = network.run([0, 1, 0]);
+    console.log(output);
+  }
+
   handleSubmit(event) {
     event.preventDefault();
     console.log("Submitted!");
-    this.props.addWine(this.state);
-    this.props.history.push("/winePredictor");
+    // this.props.addWine(this.state);
+    // this.props.history.push("/winePredictor");
+    this.trainData();
   }
+
   render() {
     return (
       <div>
