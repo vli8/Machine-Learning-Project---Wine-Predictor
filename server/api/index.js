6128a9f26e96e113f4b4736d3c71bacce7f7c238
vli8 <vli8@fordham.edu>
2019-04-18 13:42:47 -0400

    trained data switched in backend

--- server/api/index.js
+++ server/api/index.js
@@ -1,5 +1,9 @@
 const router = require("express").Router();
 const { Wines, NewWines } = require("../db/index");
+const Sequelize = require("sequelize");
+const Op = Sequelize.Op;
+const brain = require("brain.js");
+/*-----------------------------------------------------------------*/
 
 router.get("/", async (req, res, next) => {
   try {
@@ -9,6 +13,41 @@ router.get("/", async (req, res, next) => {
     console.log("ERROR", error);
   }
 });
+
+/*-----------------------------------------------------------------*/
+
+router.get("/winePrediction", async (req, res, next) => {
+  try {
+    const allWines = await Wines.findAll({
+      where: {
+        country: {
+          [Op.ne]: null
+        }
+      }
+    });
+
+    const network = new brain.recurrent.LSTM();
+    const trainingData = [];
+    for (let i = 0; i < 30; i++) {
+      trainingData.push({
+        input: allWines[i].description,
+        output: allWines[i].country
+      });
+    }
+    network.train(trainingData, { iterations: 10 });
+    console.log("finished training Data!!!");
+    const predictedCountry = network.run(
+      "Raw black-cherry aromas are direct and simple but good. This has a juicy feel that thickens over time, with oak character and extract becoming more apparent. A flavor profile driven by dark-berry fruits and smoldering oak finishes meaty but hot."
+    );
+    console.log(predictedCountry);
+    res.send(trainingData);
+  } catch (error) {
+    console.log("error in wine prediction route", error);
+  }
+});
+
+/*-----------------------------------------------------------------*/
+
 router.get("/:id", async (req, res, next) => {
   try {
     const singleWine = await Wines.findAll({
@@ -21,6 +60,9 @@ router.get("/:id", async (req, res, next) => {
     console.log("error in :id route", error);
   }
 });
+
+/*-----------------------------------------------------------------*/
+
 router.post("/addWine", async (req, res, next) => {
   try {
     console.log(req.body);
